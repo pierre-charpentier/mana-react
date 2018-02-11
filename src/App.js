b70e1bc42eed8e44bf9261692e1e7b7373acb42b
Pierre Charpentier <charpentier.pierre@outlook.com>
2018-02-11 09:03:50 +0100

    Update App.js syntax and formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Agenda from './scenes/Agenda'
 import Grades from './scenes/Grades'
 import NavigationBar from './components/NavigationBar'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-        <div className="App">
-          <NavigationBar />
-          <Switch>
-            <Route exact path='/' component={ Agenda } />
-            <Route path='/agenda' component={ Agenda } />
-            <Route path='/grades' component={ Grades } />
-          </Switch>
-        </div>
-    );
+      <div className='App'>
+        <NavigationBar />
+        <Switch>
+          <Route exact path='/' component={Agenda} />
+          <Route path='/agenda' component={Agenda} />
+          <Route path='/grades' component={Grades} />
+        </Switch>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
